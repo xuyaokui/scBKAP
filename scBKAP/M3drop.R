de3bac180b5e9f317bbb49280439a64713458373
QUST-AIBBDRC <38091066+QUST-AIBBDRC@users.noreply.github.com>
2020-12-01 17:33:46 +0800

    Update M3drop.R

--- scBKAP/M3drop.R
+++ scBKAP/M3drop.R
@@ -1,10 +1,10 @@
 library(M3Drop)
 
-b <- read.csv('ya.csv',header = F)
+b <- read.csv('ting_auto.csv',header = F)
 Normalized_data <- M3DropCleanData(b, 
                                    is.counts=TRUE, 
-                                   min_detected_genes=70)
+                                   min_detected_genes=112)
 #the min_detected_genes in each dataset is different,because of different expression values
 c <- Normalized_data$data
 c <- t(c)
-write.csv(c,'yan_m3.csv',row.names = F, col.names = F)
+write.csv(c,'ting_m3.csv',row.names = F, col.names = F)
